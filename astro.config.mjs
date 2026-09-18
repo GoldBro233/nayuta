c066384f65ce7b15aef444383f8eac04043f700c
Goldbro233 <bowensun_06@outlook.com>
2026-09-19 01:52:25 +0800

    feat(sitemap): generating sitemap.xml and rss.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,32 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { unified } from '@astrojs/markdown-remark';
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/utils/reading-time';
+import config from './src/config';
+
+const site = config.site_url;
+if (!site) {
+  throw new Error('site_url in src/config.ts must be an absolute HTTP(S) URL.');
+}
+
+let siteUrl;
+
+try {
+  siteUrl = new URL(site);
+} catch {
+  throw new Error('site_url in src/config.ts must be an absolute HTTP(S) URL.');
+}
+
+if (siteUrl.protocol !== 'http:' && siteUrl.protocol !== 'https:') {
+  throw new Error('site_url in src/config.ts must be an absolute HTTP(S) URL.');
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site,
+  integrations: [mdx(), sitemap()],
   markdown: {
     processor: unified({ remarkPlugins: [remarkReadingTime] }),
   },
